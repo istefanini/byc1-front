cfbfb6d270136ce86c3db0313f2658455f0c4b4f
Igna Stef <istefanini@fleni.org.ar>
2023-03-06 16:59:50 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+	
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+	AppComponent,
+	FileUploadComponent,
+],
+imports: [
+	BrowserModule,
+	HttpClientModule,
+	BrowserAnimationsModule,
+	MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCardModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/file-upload.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
+@Injectable({
+providedIn: 'root'
+})
+export class FileUploadService {
+	
+// API url
+baseApiUrl = "localhost:3000/sendfilebyc"
+	
+constructor(private http:HttpClient) { }
+
+// Returns an observable
+upload(file: File):Observable<any> {
+	// Create form data
+	const formData = new FormData();
+	// Store form name as "file" with file data
+	formData.append("myfilebyc", file);
+	// Make http post request over api
+	// with formData as req
+	return this.http.post(this.baseApiUrl, file)
+}
+
+  uploadFile(file: File): Observable<any> {
+	const formData = new FormData();
+	formData.append('myfilebyc', file);
+	return this.http.post<any>(`${this.baseApiUrl}`, formData, { observe: 'response' });
+  }
+
+}
